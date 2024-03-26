e6f9624f6ec58d9e1538aa73f9bb2332efe7a84d
sÒwÓs <115623421+NhatNam15151515@users.noreply.github.com>
2024-03-26 20:49:51 +0700

    Thêm nhập xuất

--- QuanLyKhoHangLaptop/src/controller/LabelController.java
+++ QuanLyKhoHangLaptop/src/controller/LabelController.java
@@ -1,34 +1,45 @@
 package controller;
 
-
 import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+
 import javax.swing.JLabel;
 
+import view.PhieuXuat;
+
 public class LabelController {
-    private static JLabel lastClickedLabel;
+	private static JLabel lastClickedLabel;
+	private static PhieuXuat PhieuXuat = new PhieuXuat();
 
-    public static void addMouseListener(JLabel label) {
-        label.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                JLabel clickedLabel = (JLabel) e.getSource();
+	public static void addMouseListener(JLabel label) {
+		label.addMouseListener(new MouseAdapter() {
 
-                // Nếu có JLabel được nhấn trước đó, đặt lại màu sắc bình thường
-                if (lastClickedLabel != null) {
-                    lastClickedLabel.setForeground(Color.WHITE);
-                    lastClickedLabel.setBackground(null);
-                }
+			@Override
 
-                // Đặt màu sắc mới cho JLabel vừa được nhấn
-                clickedLabel.setForeground(Color.BLACK);
-                clickedLabel.setBackground(Color.GRAY);
+			public void mouseClicked(MouseEvent e) {
+				JLabel clickedLabel = (JLabel) e.getSource();
+				// Nếu có JLabel được nhấn trước đó, đặt lại màu sắc bình thường
+				if (lastClickedLabel != null) {
+					lastClickedLabel.setForeground(Color.WHITE);
+					lastClickedLabel.setBackground(null);
+				}
 
-                // Cập nhật lastClickedLabel
-                lastClickedLabel = clickedLabel;
-            }
-        });
-    }
+				// Đặt màu sắc mới cho JLabel vừa được nhấn
+				clickedLabel.setForeground(Color.BLACK);
+				clickedLabel.setBackground(Color.GRAY);
+				//thêm nhập xuất
+				if (clickedLabel.getText() == "Phiếu nhập") {
+					PhieuXuat.ChangeInformationToPhieuNhap();
+					PhieuXuat.setVisible(true);
+				}
+				if (clickedLabel.getText() == "Phiếu xuất") {
+					PhieuXuat.setVisible(true);
+				}
+				// Cập nhật lastClickedLabel
+				lastClickedLabel = clickedLabel;
+			}
+		});
+	}
+
 }
-
--- QuanLyKhoHangLaptop/src/view/login.java
+++ QuanLyKhoHangLaptop/src/view/login.java
@@ -33,7 +33,6 @@ public class login extends JFrame {
 	private JTextField textField_username;
 	private JTextField textField_password;
 
-	
 	public static void main(String[] args) {
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -43,48 +42,46 @@ public class login extends JFrame {
 		}
 	}
 
-	
 	public login() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(650,450);
+		this.setSize(650, 450);
 		this.setLocationRelativeTo(null);
 		this.setTitle("Login");
-		getContentPane().setLayout(new GridLayout(1,2));
-		
-		
+		getContentPane().setLayout(new GridLayout(1, 2));
+
 		JPanel jpanel_img = new JPanel();
 		jpanel_img.setBackground(new Color(0, 128, 192));
 		getContentPane().add(jpanel_img);
-		
+
 		JPanel jpanel_login = new JPanel();
 		jpanel_login.setForeground(new Color(64, 128, 128));
 		jpanel_login.setBackground(new Color(255, 255, 255));
 		getContentPane().add(jpanel_login);
 		jpanel_login.setLayout(null);
-		
+
 		JLabel jtextFiled_username = new JLabel("UserName");
 		jtextFiled_username.setForeground(new Color(0, 0, 0));
 		jtextFiled_username.setFont(new Font("Arial", Font.BOLD, 20));
 		jtextFiled_username.setBounds(21, 93, 119, 24);
 		jpanel_login.add(jtextFiled_username);
-		
+
 		JLabel jtextFiled_username_1 = new JLabel("PassWord");
 		jtextFiled_username_1.setForeground(new Color(0, 0, 0));
 		jtextFiled_username_1.setFont(new Font("Arial", Font.BOLD, 20));
 		jtextFiled_username_1.setBounds(21, 183, 119, 24);
 		jpanel_login.add(jtextFiled_username_1);
-		
+
 		ImageIcon icon = new ImageIcon("G:\\My Drive\\DoAnJava\\QuanLyKhoHangLaptop\\src\\img\\login_mini.png");
 		jpanel_img.setLayout(null);
 		JLabel jLabel_img = new JLabel(icon);
 		jLabel_img.setBounds(84, 230, 128, 128);
 		jpanel_img.add(jLabel_img);
-		
+
 		ImageIcon icon_user = new ImageIcon("G:\\My Drive\\DoAnJava\\QuanLyKhoHangLaptop\\src\\img\\user.png");
 		JLabel jLabel_user = new JLabel(icon_user);
 		jLabel_user.setBounds(84, 102, 128, 128);
 		jpanel_img.add(jLabel_user);
-		
+
 		textField_username = new JTextField();
 		textField_username.setBackground(new Color(255, 255, 255));
 		textField_username.setForeground(new Color(0, 0, 0));
@@ -93,7 +90,7 @@ public class login extends JFrame {
 		textField_username.setOpaque(true);
 		jpanel_login.add(textField_username);
 		textField_username.setColumns(10);
-		
+
 		textField_password = new JTextField();
 		textField_password.setBackground(new Color(255, 255, 255));
 		textField_password.setForeground(new Color(0, 0, 0));
@@ -102,21 +99,20 @@ public class login extends JFrame {
 		textField_password.setBounds(21, 217, 264, 33);
 		textField_password.setOpaque(true);
 		jpanel_login.add(textField_password);
-		
-		
+
 		JLabel jLabel = new JLabel("Quên mật khẩu ?");
 		jLabel.setForeground(new Color(0, 0, 0));
 		jLabel.setFont(new Font("Arial", Font.BOLD, 16));
 		jLabel.setBounds(86, 352, 134, 24);
 		jpanel_login.add(jLabel);
-		
+
 		JButton btnLogin = new JButton("Đăng nhập");
-        btnLogin.setForeground(new Color(0, 0, 0));
-        btnLogin.setFont(new Font("Arial", Font.PLAIN, 20));
-        btnLogin.setBounds(21, 287, 264, 41);
-        jpanel_login.add(btnLogin);
-        btnLogin.setBackground(new Color(0, 128, 192));
-       
+		btnLogin.setForeground(new Color(0, 0, 0));
+		btnLogin.setFont(new Font("Arial", Font.PLAIN, 20));
+		btnLogin.setBounds(21, 287, 264, 41);
+		jpanel_login.add(btnLogin);
+		btnLogin.setBackground(new Color(0, 128, 192));
+
 		this.setVisible(true);
 	}
 }
